47d42cd51f5f02681a36b5912043a7199f748102
Tharun R <tharunr2121@gmail.com>
2026-07-09 21:26:27 +0530

    fix: resolve hobbies folder components rendering, define missing CSS variables and gradients

--- src/components/hobbies/HobbiesChapters.tsx
+++ src/components/hobbies/HobbiesChapters.tsx
@@ -1,20 +1,24 @@
-import {
-  HobbyRubiksCube,
-  HobbyTroubleshooting,
-  HobbyContentCreator,
-  HobbyDancer,
-  HobbyVideoEditor,
-  HobbyPhilosophy,
-} from "@/components/hobbies/HobbySections";
+import HobbyPCBuilding from "@/components/hobbies/HobbyPCBuilding";
+import HobbyTroubleshooting from "@/components/hobbies/HobbyTroubleshooting";
+import HobbyPCB from "@/components/hobbies/HobbyPCB";
+import HobbyPlayStation from "@/components/hobbies/HobbyPlayStation";
+import HobbyOrigami from "@/components/hobbies/HobbyOrigami";
+import Hobby3DArtist from "@/components/hobbies/Hobby3DArtist";
+import HobbyArtist2D from "@/components/hobbies/HobbyArtist2D";
+import HobbySkater from "@/components/hobbies/HobbySkater";
+import HobbyPhilosophy from "@/components/hobbies/HobbyPhilosophy";
 
 export default function HobbiesChapters() {
   return (
     <>
-      <HobbyRubiksCube />
+      <HobbyPCBuilding />
       <HobbyTroubleshooting />
-      <HobbyContentCreator />
-      <HobbyDancer />
-      <HobbyVideoEditor />
+      <HobbyPCB />
+      <HobbyPlayStation />
+      <HobbyOrigami />
+      <Hobby3DArtist />
+      <HobbyArtist2D />
+      <HobbySkater />
       <HobbyPhilosophy />
     </>
   );
